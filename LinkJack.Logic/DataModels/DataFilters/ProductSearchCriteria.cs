217da6e6a85d919f4229cd4d1d831eb61251f6eb
Raza Imam <raza.imam1992@gmail.com>
2016-08-21 17:06:48 +0530

    Logic and Data layers added.

---
+++ LinkJack.Logic/DataModels/DataFilters/ProductSearchCriteria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkJack.Logic.DataModels.DataFilters
+{
+    public class ProductSearchCriteria
+    {
+        public string ProductId { get; set; }
+        public string CategoryId { get; set; }
+        public bool isProductIdSpecified { get; set; }
+        public bool isCategoryIdSpecified { get; set; }
+
+    }
+}
---
+++ LinkJack/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LinkJack.Logic.DataModels.Data;
+using LinkJack.Logic.DataModels.DataFilters;
+
+namespace LinkJack.Controllers
+{
+    [RoutePrefix("ProductService")]
+    public class ProductController : ApiController
+    {
+        [Route("GetProduct")]
+        [HttpPost]
+        public List<ProductData> GetProduct(ProductSearchCriteria prdSC)
+        {
+            return  null;           
+        
+        }
+    }
+}
